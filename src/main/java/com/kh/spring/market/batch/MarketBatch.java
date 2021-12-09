d270cf1dd1b9fa99275a849c10534bbd1e3063dc
Kimpossible <Kimpossible94@naver.com>
2021-12-09 15:46:10 +0900

    [김영범] 오루 수정 2

--- src/main/java/com/kh/spring/market/batch/MarketBatch.java
+++ src/main/java/com/kh/spring/market/batch/MarketBatch.java
@@ -21,7 +21,7 @@ public class MarketBatch {
 
 	private final ShopService shopService;
 
-	@Scheduled(cron = "0 0 23 * * *")
+	@Scheduled(cron = "0 0 0 * * *")
 	public void updateOrderState() throws Exception {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DAY_OF_MONTH, 7);
